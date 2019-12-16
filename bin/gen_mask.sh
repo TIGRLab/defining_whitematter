21ea59c3da9ee95d2fea735f9b8c31d63db70d30
mjoseph <josephmje.22@gmail.com>
2019-12-16 13:11:09 -0500

    add left and right masks

--- bin/gen_mask.sh
+++ bin/gen_mask.sh
@@ -17,8 +17,8 @@ for SUB in $SUBJECTS; do
     # OPTIC RADIATIONS
     #########################################################
 
+    # LEFT
     # include labels 1 and 2
-
     fslmaths \
       ${MASK_DIR}/Left-Thalamus-Proper.nii.gz \
       -mul 1 ${TMP_DIR}/Left-Thalamus-Proper_thresh.nii.gz
@@ -40,7 +40,6 @@ for SUB in $SUBJECTS; do
       -mul 2 ${TMP_DIR}/ctx-lh-pericalcarine_thresh.nii.gz
 
     # exclude label 3
-
     fslmaths \
       ${MASK_DIR}/ctx-lh-parahippocampal.nii.gz \
       -mul 3 ${TMP_DIR}/ctx-lh-parahippocampal_thresh.nii.gz
@@ -57,84 +56,48 @@ for SUB in $SUBJECTS; do
       -add ${TMP_DIR}/ctx-lh-pericalcarine_thresh.nii.gz \
       -add ${TMP_DIR}/ctx-lh-parahippocampal_thresh.nii.gz \
       -add ${TMP_DIR}/Left-Hippocampus_thresh.nii.gz \
-      ${OUTPUT_DIR}/OR_mask.nii.gz
+      ${OUTPUT_DIR}/OR_left_mask.nii.gz
+
+    #RIGHT
+    # include labels 1 and 2
+    fslmaths \
+      ${MASK_DIR}/Right-Thalamus-Proper.nii.gz \
+      -mul 1 ${TMP_DIR}/Right-Thalamus-Proper_thresh.nii.gz
+
+    fslmaths \
+      ${MASK_DIR}/ctx-rh-cuneus.nii.gz \
+      -mul 2 ${TMP_DIR}/ctx-rh-cuneus_thresh.nii.gz
+
+    fslmaths \
+      ${MASK_DIR}/ctx-rh-lateraloccipital.nii.gz \
+      -mul 2 ${TMP_DIR}/ctx-rh-lateraloccipital_thresh.nii.gz
+
+    fslmaths \
+      ${MASK_DIR}/ctx-rh-lingual.nii.gz \
+      -mul 2 ${TMP_DIR}/ctx-rh-lingual_thresh.nii.gz
+
+    fslmaths \
+      ${MASK_DIR}/ctx-rh-pericalcarine.nii.gz \
+      -mul 2 ${TMP_DIR}/ctx-rh-pericalcarine_thresh.nii.gz
+
+    # exclude label 3
+
+    fslmaths \
+      ${MASK_DIR}/ctx-rh-parahippocampal.nii.gz \
+      -mul 3 ${TMP_DIR}/ctx-rh-parahippocampal_thresh.nii.gz
+
+    fslmaths \
+      ${MASK_DIR}/Right-Hippocampus.nii.gz \
+      -mul 3 ${TMP_DIR}/Right-Hippocampus_thresh.nii.gz
+
+    fslmaths \
+      ${TMP_DIR}/Right-Thalamus-Proper_thresh.nii.gz \
+      -add ${TMP_DIR}/ctx-rh-cuneus_thresh.nii.gz \
+      -add ${TMP_DIR}/ctx-rh-lateraloccipital_thresh.nii.gz \
+      -add ${TMP_DIR}/ctx-rh-lingual_thresh.nii.gz \
+      -add ${TMP_DIR}/ctx-rh-pericalcarine_thresh.nii.gz \
+      -add ${TMP_DIR}/ctx-rh-parahippocampal_thresh.nii.gz \
+      -add ${TMP_DIR}/Right-Hippocampus_thresh.nii.gz \
+      ${OUTPUT_DIR}/OR_right_mask.nii.gz
 
 done
-
-for SUB in $SUBJECTS; do
-
-    BASE_DIR=/projects/mjoseph/challenges/defining_whitematter
-    MASK_DIR=${BASE_DIR}/data/AccessoryData/Structural/${SUB}/fs_labels
-    OUTPUT_DIR=${BASE_DIR}/data/refined_tracts/${SUB}
-    TMP_DIR=${BASE_DIR}/data/refined_tracts/${SUB}/tmp
-
-    mkdir -p $OUTPUT_DIR $TMP_DIR
-
-    #########################################################
-    # ANTERIOR/POSTERIOR COMMISSURE
-    #########################################################
-
-    fslmaths \
-      # prefrontal
-      ${MASK_DIR}/ctx-lh-parstriangularis.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-parstriangularis.nii.gz \
-      -add ${MASK_DIR}/ctx-lh-parsorbitalis.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-parsorbitalis.nii.gz \
-      -add ${MASK_DIR}/ctx-lh-superiorfrontal.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-superiorfrontal.nii.gz \
-      -add ${MASK_DIR}/ctx-lh-rostralmiddlefrontal.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-rostralmiddlefrontal.nii.gz \
-      -add ${MASK_DIR}/ctx-lh-frontalpole.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-frontalpole.nii.gz \
-      -add ${MASK_DIR}/ctx-lh-lateralorbitofrontal.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-lateralorbitofrontal.nii.gz \
-      # parietal
-      -add ${MASK_DIR}/ctx-lh-postcentral.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-postcentral.nii.gz \
-      -add ${MASK_DIR}/ctx-lh-precuneus.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-precuneus.nii.gz \
-      -add ${MASK_DIR}/ctx-lh-supramarginal.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-supramarginal.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-lateralorbitofrontal.nii.gz \
-      -add ${MASK_DIR}/ctx-lh-superiorparietal.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-superiorparietal.nii.gz \
-      -add ${MASK_DIR}/ctx-lh-inferiorparietal.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-inferiorparietal.nii.gz \
-      # occipital
-      -add ${MASK_DIR}/ctx-lh-lingual.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-lingual.nii.gz \
-      -add ${MASK_DIR}/ctx-lh-pericalcarine.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-pericalcarine.nii.gz \
-      -add ${MASK_DIR}/ctx-lh-cuneus.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-cuneus.nii.gz \
-      -add ${MASK_DIR}/ctx-lh-lateraloccipital.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-lateraloccipital.nii.gz \
-      # precentral
-      -add ${MASK_DIR}/ctx-lh-precentral.nii.gz \
-      -add ${MASK_DIR}/ctx-rh-precentral.nii.gz \
-      ${TMP_DIR}/lobe_mask.nii.gz
-
-done
-
-
-    fslmaths \
-      ${MASK_DIR}/Left-Pallidum.nii.gz \
-      -mul 1 ${OUTPUT_DIR}/Left-Pallidum_thresh.nii.gz
-
-    fslmaths \
-      ${MASK_DIR}/Right-Pallidum.nii.gz \
-      -mul 1 ${OUTPUT_DIR}/Right-Pallidum_thresh.nii.gz
-
-    fslmaths \
-      ${OUTPUT_DIR}/Left-Pallidum_thresh.nii.gz \
-      -add ${OUTPUT_DIR}/Right-Pallidum_thresh.nii.gz \
-      ${OUTPUT_DIR}/ACPC_mask.nii.gz
-
-    # fslmaths \
-    #   ${OUTPUT_DIR}/Left-Caudate_thresh.nii.gz \
-    #   -add ${OUTPUT_DIR}/Left-Putamen_thresh.nii.gz \
-    #   -add ${OUTPUT_DIR}/Left-Pallidum_thresh.nii.gz \
-    #   ${OUTPUT_DIR}/ACPC_mask.nii.gz
-
-done
-
